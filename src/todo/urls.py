538edd92594c6406ddf4e1b8af5acdff374cada3
erol <hurkanrol15@gmail.com>
2021-07-14 23:48:18 +0300

    added todo project

--- src/todo/urls.py
+++ src/todo/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import home, todo_create, todo_list, todo_update
+from .views import home, todo_list, todo_update, todo_delete
 
 urlpatterns = [
     path('home/', home, name='home'),
     path('list/', todo_list, name='todo-list'),
-    path('add/', todo_create, name='todo-create'),
+    # path('add/', todo_create, name='todo-create'),
     path('<int:id>/update/', todo_update, name='todo-update'),
+    path('<int:id>/delete/', todo_delete, name='todo-delete'),
 ]
--- src/todo/views.py
+++ src/todo/views.py
@@ -8,23 +8,30 @@ def home(request):
 
 def todo_list(request):
     todos = Todo.objects.all()
-
-    context = {
-        'todos': todos,
-    }
-    return render(request, 'todo/todo_list.html', context)
-
-def todo_create(request):
     form = TodoAddForm()
     if request.method == "POST":
         form = TodoAddForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect ('todo-list')
+
     context = {
-        'form' : form,
+        'todos': todos,
+        'form': form,
     }
-    return render(request, 'todo/todo_create.html', context)
+    return render(request, 'todo/todo_list.html', context)
+
+# def todo_create(request):
+#     form = TodoAddForm()
+#     if request.method == "POST":
+#         form = TodoAddForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return redirect ('todo-list')
+#     context = {
+#         'form' : form,
+#     }
+#     return render(request, 'todo/todo_create.html', context)
 
 def todo_update(request, id):
     todo = get_object_or_404(Todo, id=id)
@@ -37,5 +44,16 @@ def todo_update(request, id):
 
     context = {
         'form': form,
+        'todo': todo,
     }
     return render(request, 'todo/todo_update.html', context)
+
+def todo_delete(request, id):
+    todo = get_object_or_404(Todo, id=id)
+    if request.method == "POST":
+        todo.delete()
+        return redirect('todo-list')
+    context = {
+        'todo': todo,
+    }
+    return render(request, 'todo/todo_delete.html', context)
